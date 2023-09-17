67b411e0781884aaaed7de4bc941de23b6e37139
NaaaRiii <axenarikilo48698062@gmail.com>
2023-09-17 14:56:44 +0900

    Create a simple home page and a sign up page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
+  #test "should get root" do
+  #  get root_url
+  #  assert_response :success
+  #  assert_select "title", "Home | Plus ONE"
+  #end
+  test "should get home" do
+    get root_path
     assert_response :success
-    assert_select "title", "Home | Plus ONE"
+    assert_select "title", "Plus ONE"
   end
 end
