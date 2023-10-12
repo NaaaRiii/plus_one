5bb1d5cc0e169fe040c846582b5b96c70ea1ea41
NaaaRiii <axenarikilo48698062@gmail.com>
2023-10-12 21:43:48 +0900

    Add guest login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root                   "static_pages#home"
-  get    "/signup",  to: "users#new"
-  get    "/login",   to: "sessions#new"
-  post   "/login",   to: "sessions#create"
-  delete "/logout",  to: "sessions#destroy"
+  root                        "static_pages#home"
+  get     "/signup",      to: "users#new"
+  get     "/login",       to: "sessions#new"
+  post    "/login",       to: "sessions#create"
+  delete  "/logout",      to: "sessions#destroy"
+  get     "/guest_login", to: "guest_sessions#create"
+  #post    "/guest_login",  to: "guest_sessions#create"
   resources :users
   resources :account_activations, only: [:edit]
 end
