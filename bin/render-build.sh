2ab04214e29269ee1b2be0ea532bb9a3c1cc50d4
NaaaRiii <axenarikilo48698062@gmail.com>
2023-10-12 19:36:09 +0900

    Create production_db

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
 ##!/usr/bin/env bash
 ## exit on error
-#set -o errexit
-#bundle install
-#bundle exec rails assets:precompile
-#bundle exec rails assets:clean
-#bundle exec rails db:migrate
+set -o errexit
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
