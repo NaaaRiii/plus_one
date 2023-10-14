6fbb612548b3d5acb203877253752d63bcc6c74d
NaaaRiii <axenarikilo48698062@gmail.com>
2023-10-14 16:35:12 +0900

    Avoided Rubocop's point

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,14 +1,25 @@
-class GuestSessionsController < ApplicationController
+# class GuestSessionsController < ApplicationController
+#  def create
+#    user = User.find_or_create_by(email: "guest@exapmle.com") do |user|
+#      user.password = SecureRandom.urlsafe_base64
+#      user.name = "ゲストユーザー"
+#    end
+#      session[:user_id] = user.id
+#      session[:session_token] = user.session_token
+#      flash[:success] = "ゲストユーザーとしてログインしました"
+#      redirect_to root_url
+#  end
+# end
 
+class GuestSessionsController < ApplicationController
   def create
-    user = User.find_or_create_by(email: "guest@exapmle.com") do |user|
+    guest_user = User.find_or_create_by(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
     end
-      session[:user_id] = user.id
-      session[:session_token] = user.session_token
-      flash[:success] = "ゲストユーザーとしてログインしました"
-      redirect_to root_url
+    session[:user_id] = guest_user.id
+    session[:session_token] = guest_user.session_token
+    flash[:success] = "ゲストユーザーとしてログインしました"
+    redirect_to root_url
   end
-
 end
