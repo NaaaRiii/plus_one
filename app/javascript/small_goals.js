e952f8e6235cd6e70158f7d9ae1b01d285a5cf40
NaaaRiii <axenarikilo48698062@gmail.com>
2023-11-14 20:32:51 +0900

    fin sort-by-latest-first

--- app/javascript/small_goals.js
+++ app/javascript/small_goals.js
@@ -1,3 +1,25 @@
+//document.addEventListener("turbo:load", function() {
+//  let counter = $("#tasks-container > div").length;
+//  console.log('JavaScriptが読み込まれました。');
+//  $("#add-task").click(function() {
+//    console.log('Add task button was clicked!');
+//    counter++;
+//    let taskField = `
+//      <div id="task-${counter}">
+//        <label for="small_goal_tasks_attributes_${counter}_content">Task</label>
+//        <input type="text" name="small_goal[tasks_attributes][${counter}][content]" id="small_goal_tasks_attributes_${counter}_content" class="task-content">
+//      </div>
+//    `;
+//    $("#tasks-container").append(taskField);
+//  });
+
+//  $("#remove-task").click(function() {
+//    if($("#tasks-container > div").length > 0) {
+//      $("#tasks-container > div:last-child").remove();
+//    }
+//  });
+//});
+
 export function setupTaskButtons() {
   const addTaskButton = document.querySelector('#add-task');
   const removeTaskButton = document.querySelector('#remove-task');
