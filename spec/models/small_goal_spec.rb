b9379879a63aaf0aebb3fb897f62acd8412bcb87
NaaaRiii <axenarikilo48698062@gmail.com>
2024-09-05 21:32:45 +0900

    fin deadline_validation

--- spec/models/small_goal_spec.rb
+++ spec/models/small_goal_spec.rb
@@ -13,8 +13,9 @@ RSpec.describe SmallGoal, type: :model do
 
   describe 'validations' do
     it 'is valid if it has a title, difficulty, deadline, and at least one task' do
-      goal = FactoryBot.create(:goal)
-      small_goal = FactoryBot.build(:small_goal, goal: goal)
+      goal = FactoryBot.create(:goal, deadline: Date.today + 10.days)
+      small_goal = FactoryBot.build(:small_goal, goal: goal, deadline: Date.today + 5.days)
+      small_goal.tasks.build(content: 'Test Task')
       expect(small_goal).to be_valid
     end
 
@@ -56,5 +57,21 @@ RSpec.describe SmallGoal, type: :model do
       expect(small_goal).not_to be_valid
       expect(small_goal.errors[:base]).to include("You must have at least one task.")
     end
+
+    it 'is invalid if the small goal deadline is after the goal deadline' do
+      goal = FactoryBot.create(:goal, deadline: Date.today + 7.days)
+      small_goal = FactoryBot.build(:small_goal, goal: goal, deadline: Date.today + 8.days)
+      small_goal.tasks.build(content: 'Test Task')
+      expect(small_goal).not_to be_valid
+      expect(small_goal.errors[:base]).to include("Goalよりも後の日付を設定することはできません")
+    end
+
+    it 'is valid if the small goal deadline is on or before the goal deadline' do
+      goal = FactoryBot.create(:goal, deadline: Date.today + 7.days)
+      small_goal = FactoryBot.build(:small_goal, goal: goal, deadline: Date.today + 7.days)
+      small_goal.tasks.build(content: 'Test Task')
+      expect(small_goal).to be_valid
+    end
+
   end
 end
