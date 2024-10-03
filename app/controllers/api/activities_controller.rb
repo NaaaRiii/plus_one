dab6576631c1698f1a34ce3775c7250b72534ed9
NaaaRiii <axenarikilo48698062@gmail.com>
2024-10-03 11:08:20 +0900

    fin past_exp_earned

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -25,36 +25,40 @@ module Api
       render json: date_range
     end
 
-    
-
     #def daily_exp
     #  start_of_month = Date.today.beginning_of_month
-    #  end_of_month = Date.today.end_of_month
-
+    #  end_of_month = Date.today.end_of_month.end_of_day
+    
     #  activities = current_user.activities.where(completed_at: start_of_month..end_of_month)
     #  exp_by_day = activities.group_by_day(:completed_at, time_zone: 'Asia/Tokyo').sum(:exp_gained)
-
+    
+    #  # デバッグ情報を追加
+    #  puts "Start of month: #{start_of_month}"
+    #  puts "End of month: #{end_of_month}"
+    #  puts "Activities: #{activities.inspect}"
+    #  puts "Exp by day: #{exp_by_day.inspect}"
+    
     #  formatted_exp_by_day = {}
     #  (start_of_month..end_of_month).each do |date|
     #    formatted_date = date.strftime("%Y-%m-%d")
     #    formatted_exp_by_day[formatted_date] = exp_by_day[date] || 0
     #  end
-
+    
     #  exp_by_day.each do |key, value|
     #    formatted_key = key.is_a?(Date) ? key.strftime("%Y-%m-%d") : key
     #    formatted_exp_by_day[formatted_key] ||= value
     #  end
-
+    
     #  sorted_exp_by_day = formatted_exp_by_day.sort_by { |key, _value| key }.to_h
-
+    
     #  render json: sorted_exp_by_day
     #end
-    
+
     def daily_exp
-      start_of_month = Date.today.beginning_of_month
-      end_of_month = Date.today.end_of_month.end_of_day
+      start_of_month = 3.months.ago.beginning_of_month.to_date
+      end_of_month = [Date.today, Date.today.end_of_month.to_date].min
     
-      activities = current_user.activities.where(completed_at: start_of_month..end_of_month)
+      activities = current_user.activities.where(completed_at: start_of_month.beginning_of_day..end_of_month.end_of_day)
       exp_by_day = activities.group_by_day(:completed_at, time_zone: 'Asia/Tokyo').sum(:exp_gained)
     
       # デバッグ情報を追加
@@ -78,5 +82,6 @@ module Api
     
       render json: sorted_exp_by_day
     end
+
   end
 end
