b2b233c11e9aef9c1d2151f71b87369272e37c5f
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-17 14:35:06 +0900

    fin fix_user_model_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true, on: :create
 
   # JWTトークンを生成するメソッド
   def generate_auth_token
