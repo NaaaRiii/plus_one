91113f6c118e1959dd5b779027a74d278d32f953
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-17 12:01:12 +0900

    fin fix_models_activity_test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "Do Test" }
-    email { "john@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
   end
 end
