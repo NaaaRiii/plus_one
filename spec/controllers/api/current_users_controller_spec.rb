d02efeec5a8cf75746284d5505ae1a6a155fe089
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-17 23:00:57 +0900

    fin fix_current-users-controller_test2

--- spec/controllers/api/current_users_controller_spec.rb
+++ spec/controllers/api/current_users_controller_spec.rb
@@ -103,5 +103,180 @@ RSpec.describe Api::CurrentUsersController, type: :controller do
       # tickets
       expect(json['tickets']).to eq 7
     end    
+
+    it 'latestCompletedGoals が 24h 以内の 3 件になること' do
+      user  = create(:user)
+      goal  = create(:goal, user: user)
+
+      # 24h 以内に完了した SmallGoal を3件
+      recent_goals = []
+      3.times do |i|
+        t = (i + 1).hours.ago
+        recent_goals << create(:small_goal,
+                               goal: goal,
+                               completed: true,
+                               completed_time: t,
+                               deadline: t + 2.days)
+      end
+
+      # 24h より前に完了した SmallGoal を10件
+      10.times do |i|
+        t = (25 + i).hours.ago
+        create(:small_goal,
+               goal: goal,
+               completed: true,
+               completed_time: t,
+               deadline: t + 2.days)
+      end
+
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      get :show
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body)
+      latest = json['latestCompletedGoals']
+      expect(latest.size).to eq 3
+
+      expected_ids = recent_goals.sort_by(&:completed_time).reverse.map(&:id)
+      actual_ids   = latest.map { |g| g['id'] }
+      expect(actual_ids).to eq expected_ids
+    end
+
+    it '24h 内に完了が無い場合は最新の完了 SmallGoal 10 件が返ること' do
+      user = create(:user)
+      goal = create(:goal, user: user)
+
+      old_goals = []
+      11.times do |i|
+        t = (25 + i).hours.ago
+        old_goals << create(:small_goal,
+                            goal: goal,
+                            completed: true,
+                            completed_time: t,
+                            deadline: t + 2.days)
+      end
+
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      get :show
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body)
+      latest = json['latestCompletedGoals']
+      expect(latest.size).to eq 10
+
+      # newest 10 of old_goals (descending by completed_time)
+      expected_ids = old_goals.sort_by(&:completed_time).reverse.map(&:id).first(10)
+      actual_ids = latest.map { |g| g['id'] }
+      expect(actual_ids).to eq expected_ids
+    end
+
+    it 'latestCompletedGoals が completed_time 降順で返ること (7件ケース)' do
+      user = create(:user)
+      goal = create(:goal, user: user)
+
+      recent = []
+      7.times do |i|
+        t = (i + 1).hours.ago
+        recent << create(:small_goal,
+                         goal: goal,
+                         completed: true,
+                         completed_time: t,
+                         deadline: t + 2.days)
+      end
+
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      get :show
+      expect(response).to have_http_status(:ok)
+
+      json   = JSON.parse(response.body)
+      latest = json['latestCompletedGoals']
+      expect(latest.size).to eq 7
+
+      times = latest.map { |g| g['completed_time'] }
+      expect(times).to eq times.sort.reverse
+      expect(latest.first['id']).to eq recent.sort_by(&:completed_time).last.id
+    end
+
+    it '@current_user が nil の場合は 404 と JSON {error:"User not found"} が返ること' do
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, nil)
+
+      get :show
+      expect(response).to have_http_status(:not_found)
+      expect(response.content_type).to include('application/json')
+      json = JSON.parse(response.body)
+      expect(json.keys).to eq(['error'])
+      expect(json['error']).to eq('User not found')
+    end
+
+    it 'update_rank が成功すると JSON {success:true} かつ DB が更新されること' do
+      user = create(:user, last_roulette_rank: 0)
+
+      # 認証フィルタをバイパス
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      patch :update_rank, params: { id: user.id, new_rank: 7 }
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json).to eq('success' => true, 'message' => 'Rank updated successfully.')
+
+      expect(user.reload.last_roulette_rank).to eq 7
+    end
+
+    it 'new_rank が文字列の場合 to_i で 0 がセットされること' do
+      user = create(:user, last_roulette_rank: 5)
+
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      patch :update_rank, params: { id: user.id, new_rank: 'abc' }
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json['success']).to be true
+
+      expect(user.reload.last_roulette_rank).to eq 0
+    end
+
+    it 'update_rank が失敗すると 422 と JSON {success:false} が返ること' do
+      user = create(:user, last_roulette_rank: 1)
+
+      allow(controller).to receive(:authenticate_user).and_return(true)
+      controller.instance_variable_set(:@current_user, user)
+
+      # update が false を返すようにスタブ
+      allow(user).to receive(:update).and_return(false)
+
+      patch :update_rank, params: { id: user.id, new_rank: 7 }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      json = JSON.parse(response.body)
+      expect(json).to eq('success' => false, 'message' => 'Failed to update rank.')
+    end
+
+    context '内部で例外が発生した場合' do
+      it '@current_user.goals が例外を投げると 500 エラーが発生すること' do
+        user = create(:user)
+
+        allow(controller).to receive(:authenticate_user).and_return(true)
+        controller.instance_variable_set(:@current_user, user)
+
+        # goals 呼び出しで例外を発生させる
+        allow(user).to receive(:goals).and_raise(StandardError.new('DB error'))
+
+        # テスト環境では show_exceptions=false のため直接例外が飛ぶ
+        expect do
+          get :show
+        end.to raise_error(StandardError, 'DB error')
+      end
+    end
   end
 end 
