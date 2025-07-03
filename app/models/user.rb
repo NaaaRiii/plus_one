3eac190fbda0a880157f1e171b87b9cf94bf4828
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-03 16:59:55 +0900

    fin fix_user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,11 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true, on: :create
 
-  # JWTトークンを生成するメソッド
   def generate_auth_token
     payload = { user_id: id }
     JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
   end
   
-  # 渡された文字列のハッシュ値を返す
   def self.digest(string)
     cost = if ActiveModel::SecurePassword.min_cost
              BCrypt::Engine::MIN_COST
@@ -37,25 +35,20 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # ランダムなトークンを返す
   def self.new_token
     SecureRandom.urlsafe_base64
   end
 
-  # 永続的セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
     remember_digest
   end
 
-  # セッションハイジャック防止のためにセッショントークンを返す
-  # この記憶ダイジェストを再利用しているのは単に利便性のため
   def session_token
     remember_digest || remember
   end
 
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
@@ -63,18 +56,15 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
 
-  # ユーザーの総合計経験値を計算する
   def add_exp(amount)
     self.total_exp += amount
     save
   end
 
-  # ランクアップに必要な経験値を計算するメソッド
   def calculate_rank_up_experience(max_rank = 121)
     experiences = [0, 5]
     increment = 10
@@ -87,12 +77,10 @@ class User < ApplicationRecord
     experiences
   end
 
-  # ランクを計算するメソッド
   def calculate_rank
     total_exp = self.total_exp || 0.0
     experiences = calculate_rank_up_experience
   
-    # ランク1の場合を特別に扱う
     return 1 if total_exp < experiences[1]
   
     experiences.each_with_index do |exp, index|
@@ -117,61 +105,36 @@ class User < ApplicationRecord
       new_rank  = calculate_rank
       new_tens  = new_rank / 10
       old_tens  = (last_roulette_rank || 0) / 10
-      inc_amount = new_tens - old_tens          # ★ ブロック内で定義
+      inc_amount = new_tens - old_tens
   
-      return if inc_amount <= 0                 # 増分なしなら何もしない
+      return if inc_amount <= 0
   
       self.tickets            += inc_amount
       self.last_roulette_rank  = new_rank
-      save!                                        # ← ここでコミット
+      save!
   
-      # ★ 参照もブロック内に置けば NameError は起きない
       Rails.logger.debug "Tickets +#{inc_amount} (rank #{last_roulette_rank})"
     end
   end
 
-  # チケットを 1 枚消費。成功なら true, 枚数不足なら false を返す
   def use_ticket
-    with_lock do                    # ① 同一レコードで行ロック
+    with_lock do
       return false unless tickets.positive?
 
-      # decrement! は UPDATE users SET tickets = tickets - 1, updated_at = NOW() ...
-      decrement!(:tickets)          # ② 成功時は true を返す
+      decrement!(:tickets)
     end
     true
   rescue ActiveRecord::RecordInvalid
-    false                           # validation に引っかかった場合も false
+    false
   end
 
-  # アカウントを有効にする
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
 
-  # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-
-  # プレイチケットを使う
-  #def use_play_ticket
-  #  if play_tickets.positive?
-  #    decrement!(:play_tickets)
-  #    true
-  #  else
-  #    false
-  #  end
-  #end
-
-  # 編集チケットを使う
-  #def use_edit_ticket
-  #  if edit_tickets.positive?
-  #    decrement!(:edit_tickets)
-  #    true
-  #  else
-  #    false
-  #  end
-  #end
 
   protected
 
@@ -198,12 +161,10 @@ class User < ApplicationRecord
 
   private
 
-  # メールアドレスをすべて小文字にする
   def downcase_email
     self.email = email.downcase
   end
 
-  # 有効化トークンとダイジェストを作成および代入する
   def create_activation_digest
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
