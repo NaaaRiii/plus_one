728da931428a511257735169f1ee5620c5b76a2a
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-05 15:57:10 +0900

    unique indexの衝突を避けるため、cognito_subにNULLを許可する

--- db/migrate/20250517003439_add_cognito_sub_to_users.rb
+++ db/migrate/20250517003439_add_cognito_sub_to_users.rb
@@ -1,6 +1,9 @@
 class AddCognitoSubToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :cognito_sub, :string, null: false, default: ""
+    # 1) まずは null を許容してカラムを追加
+    add_column :users, :cognito_sub, :string, null: true
+
+    # 2) 一意インデックスを作成
     add_index :users, :cognito_sub, unique: true
   end
 end
