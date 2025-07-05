60ae9dc4f97fbe9f8f751eb2c91e32172ceaaa3c
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-05 15:43:14 +0900

    unique indexのcognito_subマイグレーションを修正

--- db/migrate/20250517003439_add_cognito_sub_to_users.rb
+++ db/migrate/20250517003439_add_cognito_sub_to_users.rb
@@ -1,6 +1,6 @@
 class AddCognitoSubToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :cognito_sub, :string
+    add_column :users, :cognito_sub, :string, null: false, default: ""
     add_index :users, :cognito_sub, unique: true
   end
 end
