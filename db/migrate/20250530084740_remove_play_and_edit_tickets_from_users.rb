db654146abd057dbe76e7c8d1bc95e440fedb478
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-05 21:58:32 +0900

    マイグレーションファイルを修正

--- db/migrate/20250530084740_remove_play_and_edit_tickets_from_users.rb
+++ db/migrate/20250530084740_remove_play_and_edit_tickets_from_users.rb
@@ -1,4 +1,4 @@
-class RemovePlayTicketsAndEditTicketsFromUsers < ActiveRecord::Migration[7.0]
+class RemovePlayAndEditTicketsFromUsers < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :play_tickets, :integer
     remove_column :users, :edit_tickets, :integer
