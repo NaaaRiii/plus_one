3f1111bea6f78b7ecb70e66b8384e7a229e05420
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-05 16:25:27 +0900

    change_total_exp_scaleマイグレーションからサポートされていないアルゴリズムオプションを削除

--- db/migrate/20231118100743_change_total_exp_scale.rb
+++ db/migrate/20231118100743_change_total_exp_scale.rb
@@ -1,5 +1,5 @@
 class ChangeTotalExpScale < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :total_exp, :decimal, precision: 10, scale: 2, algorithm: :copy
+    change_column :users, :total_exp, :decimal, precision: 10, scale: 2
   end
 end
